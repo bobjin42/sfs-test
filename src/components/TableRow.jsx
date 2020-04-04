ea0155c7bb44d721cb27f80ebc78ba309712f8d1
Yicheng Jin <bobjin42@gmail.com>
2020-04-03 22:02:36 -0500

    formate data

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -3,13 +3,16 @@ import React from "react";
 export default function TableRow({
   rowData: { creditorName, firstName, lastName, minPaymentPercentage, balance },
 }) {
+  const formatedMinPayment = `${minPaymentPercentage.toFixed(2)}%`;
+  const formatedBalance = balance.toFixed(2);
+
   return (
     <tr>
       <td>{creditorName}</td>
       <td>{firstName}</td>
       <td>{lastName}</td>
-      <td>{minPaymentPercentage}</td>
-      <td>{balance}</td>
+      <td>{formatedMinPayment}</td>
+      <td>{formatedBalance}</td>
     </tr>
   );
 }
